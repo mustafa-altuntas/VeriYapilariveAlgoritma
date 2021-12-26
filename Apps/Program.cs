415a65a2a3b2d2fe5af806b71ebb9f59b3bb9ba4
pc <pc@DESKTOP-8R14EDI>
2021-12-26 13:15:55 +0300

    Add project files.

---
+++ Apps/Program.cs
@@ -0,0 +1,29 @@
+﻿using DataStructures.LinkedList.SinglyLinkedList;
+using System;
+
+namespace Apps
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var linkedlist = new SinglyLinkedList<int>();
+            linkedlist.AddFirst(1);
+            linkedlist.AddFirst(2);
+            linkedlist.AddFirst(3);
+            // 3 2 1 O(1)
+
+            linkedlist.AddLast(4);
+            linkedlist.AddLast(5);
+            linkedlist.AddLast(6);
+            // 3 2 1 4 5 6  O(n)
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
---
+++ DataStructures/LinkedList/SinglyLinkedList/SinglyLinkedList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.LinkedList.SinglyLinkedList
+{
+    public class SinglyLinkedList<T>
+    {
+        public SinglyLinkedListNode<T> Head { get; set; }
+
+        public void AddFirst(T value)
+        {
+            var newNode = new SinglyLinkedListNode<T>(value);
+            newNode.Next = Head;
+            Head = newNode;
+        }
+
+        public void AddLast(T value)
+        {
+            var newNode = new SinglyLinkedListNode<T>(value);
+            if (Head == null)
+            {
+                Head = newNode;
+                return;
+            }
+            var current = Head;
+            while (current.Next != null)
+                current = current.Next;
+            current.Next = newNode;
+        }
+    }
+}
